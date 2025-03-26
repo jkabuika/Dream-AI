05d5728176103ad17fb0c7efd6a6a7da3387aa24
jkabuika <jkabuika1319@gmail.com>
2025-03-26 09:50:48 -0400

    Set up JS for elements to load after HTML is loaded

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const dreamInput = document.querySelector(".enter-dream-container input")
+  const enterBtn = document.querySelector(".enter-btn")
+})
+
+function interpretDream(dreamInput) {
+
+}
+
