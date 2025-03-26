02889ddf4948364e037322e580a309d2de1cf1f1
jkabuika <jkabuika1319@gmail.com>
2025-03-26 10:31:12 -0400

    Aligned the button to the middle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dreamInput = document.querySelector(".enter-dream-container input")
-  const enterBtn = document.querySelector(".enter-btn")
+  const enterBtn = document.querySelector(".enter-btn button")
 })
 
 function interpretDream(dreamInput) {
